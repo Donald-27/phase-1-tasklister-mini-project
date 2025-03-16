47d58b364c6efea09e5c1586f1375ac8095798c3
Donald-27 <+>
2025-03-16 16:29:25 +0300

    modified  src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+    const form = document.getElementById("create-task-form");
+    const taskList = document.getElementById("tasks");
+  
+    form.addEventListener("submit", function (event) {
+      event.preventDefault(); // Prevents page from refreshing
+  
+      const inputField = document.getElementById("new-task-description");
+      const taskText = inputField.value.trim();
+  
+      if (taskText !== "") {
+        const listItem = document.createElement("li");
+        listItem.textContent = taskText;
+  //delete butoon
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "delete";
+        deleteButton.addEventListener("click", () => listItem.remove());
+  
+        listItem.appendChild(deleteButton);
+        taskList.appendChild(listItem);
+      }
+  
+      inputField.value = ""; 
+  });
+})
